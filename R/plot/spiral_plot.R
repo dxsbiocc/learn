01cd830438c99739825de0d23c05af01e5faf14c
dengxs <43510697+dxsbiocc@users.noreply.github.com>
2021-04-27 13:35:26 +0800

    Create spiral_plot.R

---
+++ R/plot/spiral_plot.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+
+#################### 螺旋柱状图 ####################
+data <- group_by(bit_data, year, month) %>%
+  summarise(value = mean(High)) %>%
+  mutate(
+    xmin = month,
+    xmax = month + 1,
+    ymin = (year - 2015) * 12 + month,
+    ymax = ymin + runif(n(), min = 3, max = 10)
+    # ymax = ymin + 12  # 螺旋热图
+  ) %>%
+  # 根据每行的四个坐标，构建成 4 行，代表 4 个点
+  # 因为 geom_polygon 是逆时针连接起来的，所以点
+  # 的顺序也要依次排列
+  rowwise() %>%
+  do(with(., tibble(
+    year = year,
+    month = month,
+    value = value,
+    x = c(xmin, xmax, xmax, xmin),
+    y = c(ymin, ymin + 1, ymax + 1, ymax)
+  )))
+
+# 在这里，设置 group 参数非常重要。我们要每个月份
+# 作为一组，绘制一个平行四边形，所以需要根据年份
+# 与月份两列来进行分组
+ggplot(data, aes(x, y, group = paste(year, month))) +
+  geom_polygon(aes(fill = value), colour = "black") +
+  coord_polar() +
+  scale_x_continuous("month", breaks = seq(1.5, 12.5, 1), labels = month.name) +
+  scale_fill_gradientn(values = seq(0,1,0.2), colours = c('cyan','blue','green','orange','red')) +
+  ylim(c(-6, 60)) +
+  theme(
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    axis.text.y = element_blank()
+  )
